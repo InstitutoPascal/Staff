666390ba194076fd25269be255414b2cbf070126
Hernan Robles <robleshernan91@gmail.com>
2017-05-20 19:32:55 -0300

    se agrego los controladores instalacionesDiaActual, instalacionesTodas, soportesDiaActual, soportesTodas, mantenimientosRealizarInforme y mantenimientosRealizados

--- controllers/tecnicos.py
+++ controllers/tecnicos.py
@@ -1,5 +1,37 @@
 # -*- coding: utf-8 -*-
 # intente algo como
+
 def inicio():
+    d = 4
+    return dict(datos=d)
+
+
+def instalacionesDiaActual():
+    d = 4
+    return dict(datos=d)
+
+def instalacionesTodas():
+    d = 4
+    return dict(datos=d)
+
+
+def soportesDiaActual():
+    d = 4
+    return dict(datos=d)
+
+
+
+def soportesTodas():
+    d = 4
+    return dict(datos=d)
+
+
+
+def mantenimientosRealizarInforme():
+    d = 4
+    return dict(datos=d)
+
+
+def mantenimientosRealizados():
     d = 4
     return dict(datos=d)
