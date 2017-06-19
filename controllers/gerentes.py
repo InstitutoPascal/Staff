15faf893136c6ad8fa1c13f6b7ce69936069f821
Hernan Robles <robleshernan91@gmail.com>
2017-06-19 18:12:29 -0300

    Agrego la vista para gerentes estadisticas.html, en el controlador la funcion estadisticas y se modifico el menu en layout_gerentes fixes #111

--- controllers/gerentes.py
+++ controllers/gerentes.py
@@ -5,3 +5,9 @@ def index(): return dict(message="hello from gerentes.py")
 def inicio():
     d = 4
     return dict(datos=d)
+
+
+def estadisticas():
+    title="Gráfico Informativo"
+    data=XML('[ ["item", "value"], ["Solicitud de instalaciones", 20], ["Cantidad de clientes", 30], ["Solicitud de soportes", 20],["Soportes                       realizados", 15],["Mantenimientos", 14]]') #convert list in string and string in XML
+    return dict(title=title, data=data)
