d4799aace1d6ec6792be96628019d1b4cff02dac
Emanuel Carabajal <emanuelcarabajal9@gmail.com>
2017-09-02 02:07:50 -0300

    modificacion administradores.py fixes #163

--- controllers/administradores.py
+++ controllers/administradores.py
@@ -1,8 +1,53 @@
 # intente algo como
 def index(): return dict(message="hello from consultas.py")
 
-def alta_instalacion():
-    form = SQLFORM(db.instalaciones)
+##################################################################
+
+def inicio():
+    d = 4
+    return dict(datos=d)
+
+##################################################################
+
+def listadoPlanes():
+    datosPlanes = db().select(db.planes.ALL)
+    i=0
+    for x in datosPlanes:
+         i=i+1
+    return dict (datos=datosPlanes, cantidad=i)
+
+def listadoCostos_instalaciones():
+    datosCostos_instalaciones = db().select(db.costos_instalaciones.ALL)
+    i=0
+    for x in datosCostos_instalaciones:
+         i=i+1
+    return dict (datos=datosCostos_instalaciones, cantidad=i)
+
+def listadoCostos_soportes():
+    datosCostos_soportes = db().select(db.costos_soportes.ALL)
+    i=0
+    for x in datosCostos_soportes:
+         i=i+1
+    return dict (datos=datosCostos_soportes, cantidad=i)
+
+def listadoNodos():
+    datosNodos = db(db.nodos.localidad==db.localidades.id).select(db.nodos.ALL, db.localidades.ALL)
+    i=0
+    for x in datosNodos:
+         i=i+1
+    return dict (datos=datosNodos, cantidad=i)
+
+def listadoPaneles():
+    datosPaneles = db(db.paneles.nodo==db.nodos.id).select(db.paneles.ALL, db.nodos.ALL)
+    i=0
+    for x in datosPaneles:
+         i=i+1
+    return dict (datos=datosPaneles, cantidad=i)
+
+##################################################################
+
+def alta_solicitud_instalacion():
+    form = SQLFORM(db.solicitudes_instalacion)
     if form.accepts(request.vars, session):
         redirect(URL(c="administradores", f="actualizar_coords"))
         response.flash = 'Formulario aceptado'
@@ -11,6 +56,22 @@ def alta_instalacion():
     else:
         response.flash = 'Complete el formulario'
     return dict(f=form)
+
+def listadoSolicitudes_instalacion():
+    datosSolicitudes = db((db.solicitudes_instalacion.localidad == db.localidades.id)&(db.solicitudes_instalacion.tipo_de_plan == db.planes.id)&(db.solicitudes_instalacion.costo_de_instalacion==db.costos_instalaciones.id)&(db.solicitudes_instalacion.tecnico_asignado==db.tecnicos.id)).select(db.solicitudes_instalacion.ALL, db.localidades.ALL, db.planes.ALL, db.costos_instalaciones.ALL, db.tecnicos.ALL)
+    i=0
+    for x in datosSolicitudes:
+         i=i+1
+    return dict (datos=datosSolicitudes, cantidad=i)
+
+##################################################################
+
+def solicitarSoporte():
+    i=0
+    instalacion =db().select(db.solicitudes_instalacion.ALL)
+    for x in instalacion:
+        i= i+1
+    return dict(datos=instalacion, cantidad=i)
 
 def alta_soportes():
     form = SQLFORM(db.soportes_tecnicos)
@@ -22,8 +83,60 @@ def alta_soportes():
         response.flash = 'Complete el formulario'
     return dict(f=form)
 
+def listadoSoportes():
+    datosSoportes = db((db.soportes_tecnicos.tecnico_asignado==db.tecnicos.id) & (db.soportes_tecnicos.cliente==db.clientes.id)).select(db.soportes_tecnicos.ALL, db.tecnicos.ALL, db.clientes.ALL)
+    i=0
+    for x in datosSoportes:
+         i=i+1
+    return dict (datos=datosSoportes, cantidad=i)
 
+def listadoHistoriales():
+    datosHistorial = db().select(db.historiales.ALL)
+    i=0
+    for x in datosHistorial:
+         i=i+1
+    return dict (datos=datosHistorial, cantidad=i)
 
+##################################################################
+
+def clientes_dni():
+    dni_recibido=request.vars.dni
+    resultado = db((db.clientes.dni == dni_recibido)&(db.clientes.localidad==db.localidades.id)).select(db.clientes.ALL, db.localidades.ALL)
+    if resultado:
+        return dict(datos= resultado)
+    else:
+        return dict(datos=0)
+
+def clientes_nombre_apellido():
+    nom=request.vars.nom_ape
+    if nom != None:
+        resultado = db(
+            ((db.clientes.nombre.lower()==nom.lower())&(db.clientes.localidad == db.localidades.id)) |
+            ((db.clientes.apellido.lower()==nom.lower())&(db.clientes.localidad == db.localidades.id)) |
+            ((db.clientes.nombre.lower() + ' ' + db.clientes.apellido.lower() == nom.lower())&(db.clientes.localidad == db.localidades.id)) |
+            ((db.clientes.nombre.lower().contains(nom.lower()))&(db.clientes.localidad == db.localidades.id)) |
+            ((db.clientes.apellido.lower().contains(nom.lower()))&(db.clientes.localidad == db.localidades.id))).select(db.clientes.ALL, db.localidades.ALL)
+        if resultado:
+            return dict(datos= resultado)
+        else:
+            return dict(datos=0)
+    else:
+        return dict(datos="None")
+
+def clientes_ip():
+    #Se debe recibir el dni, desde la vista y devolver el registro.
+    i=0
+    cliente =db().select(db.clientes.ALL)
+    for x in cliente:
+        i= i+1
+    return dict(datos=cliente, cantidad=i)
+
+def listadoClientes():
+    datosClientes = db((db.clientes.localidad==db.localidades.id)&(db.clientes.panel==db.paneles.id)&(db.clientes.tipo_de_plan==db.planes.id)).select(db.clientes.ALL, db.localidades.localidad, db.paneles.panel, db.planes.ALL)
+    i=0
+    for x in datosClientes:
+         i=i+1
+    return dict (datos=datosClientes, cantidad=i)
 
 def coords_by_address(direccion):
     import re, urllib
@@ -50,23 +163,14 @@ def coords_by_address(direccion):
 
 def actualizar_coords():
     ret = ""
-    criterios = db.instalaciones.localidad == db.localidades.id
+    criterios = db.solicitudes_instalacion.localidad == db.localidades.id
     #criterios &= db.clientes.latitud == 0
-    for reg in db(criterios).select(db.instalaciones.id, db.instalaciones.direccion, db.instalaciones.numero_de_calle, db.localidades.localidad, db.localidades.codigo_postal):
-        dom = "%s %s, %s, %s, %s" % (reg.instalaciones.direccion, reg.instalaciones.numero_de_calle, reg.localidades.localidad,"buenos aires","argentina")
+    for reg in db(criterios).select(db.solicitudes_instalacion.id, db.solicitudes_instalacion.direccion, db.solicitudes_instalacion.numero_de_calle, db.localidades.localidad, db.localidades.codigo_postal):
+        dom = "%s %s, %s, %s, %s" % (reg.solicitudes_instalacion.direccion, reg.solicitudes_instalacion.numero_de_calle, reg.localidades.localidad,"buenos aires","argentina")
         lat, lon , url = coords_by_address(dom)
-        db(db.instalaciones.id==reg.instalaciones.id).update(latitud=lat, longitud=lon)
-        ret += "solicitante: %s coords= %s,%s url: %s\n\r" % (reg.instalaciones.id, lat, lon, url)
-    redirect(URL(c="administradores", f="alta_instalacion"))
-
-
-def inicio():
-    d = 4
-    return dict(datos=d)
-
-def geolocalizacionNodos():
-    d = 4
-    return dict(datos=d)
+        db(db.solicitudes_instalacion.id==reg.solicitudes_instalacion.id).update(latitud=lat, longitud=lon)
+        ret += "solicitante: %s coords= %s,%s url: %s\n\r" % (reg.solicitudes_instalacion.id, lat, lon, url)
+    redirect(URL(c="administradores", f="alta_solicitud_instalacion"))
 
 def geolocalizacionClientes():
     rows=db((db.clientes.id>0)&(db.clientes.localidad==db.localidades.id)).select(
@@ -81,53 +185,17 @@ def geolocalizacionClientes():
     d = dict(x0=x0,y0=y0,rows=rows)
     return response.render(d)
 
-def clientes_dni():
-    dni_recibido=request.vars.dni
-    resultado = db((db.clientes.dni == dni_recibido)&(db.clientes.localidad==db.localidades.id)).select(db.clientes.ALL, db.localidades.ALL)
-    if resultado:
-        return dict(datos= resultado)
-    else:
-        return dict(datos=0)
+def geolocalizacionNodos():
+    d = 4
+    return dict(datos=d)
 
-def solicitarSoporte():
-    #recibir dni, comparar con los registros y devolver
-    i=0
-    instalacion =db().select(db.instalaciones.ALL)
-    for x in instalacion:
-        i= i+1
-    return dict(datos=instalacion, cantidad=i)
-    
-def clientes_nombre_apellido():
-    nom=request.vars.nom_ape
-    if nom != None:
-        resultado = db(
-            ((db.clientes.nombre.lower()==nom.lower())&(db.clientes.localidad == db.localidades.id)) |
-            ((db.clientes.apellido.lower()==nom.lower())&(db.clientes.localidad == db.localidades.id)) |
-            ((db.clientes.nombre.lower() + ' ' + db.clientes.apellido.lower() == nom.lower())&(db.clientes.localidad == db.localidades.id)) |
-            ((db.clientes.nombre.lower().contains(nom.lower()))&(db.clientes.localidad == db.localidades.id)) |
-            ((db.clientes.apellido.lower().contains(nom.lower()))&(db.clientes.localidad == db.localidades.id))).select(db.clientes.ALL, db.localidades.ALL)
-        if resultado:
-            return dict(datos= resultado)
-        else:
-            return dict(datos=0)
-    else:
-        return dict(datos="None")
-
-def clientes_ip():
-    #Se debe recibir el dni, desde la vista y devolver el registro.
-    i=0
-    cliente =db().select(db.clientes.ALL)
-    for x in cliente:
-        i= i+1
-    return dict(datos=cliente, cantidad=i)
-
-def editar_instalacion():
-    id_instalacion = request.args[0]                                           # obtengo el primer argumento (ver URL)
-    solicitud =  db(db.instalaciones.id == id_instalacion).select().first()    # busco el registro en la bbdd
-    form=SQLFORM(db.instalaciones, solicitud)                                  # armo el formulario para modificar este registro:
+def editar_solicitud_instalacion():
+    id_solicitud = request.args[0]                                           # obtengo el primer argumento (ver URL)
+    solicitud =  db(db.solicitudes_instalacion.id == id_solicitud).select().first()    # busco el registro en la bbdd
+    form=SQLFORM(db.solicitudes_instalacion, solicitud)                                  # armo el formulario para modificar este registro:
     if form.accepts(request.vars, session):
         session.flash = 'Formulario correctamente cargado'
-        redirect(URL(c="consultas", f="listado_clientes"))
+        redirect(URL(c="consultas", f="listadoClientes"))
     elif form.errors:
 		response.flash = 'Su formulario contiene errores, porfavor modifiquelo'
     else: 
@@ -147,81 +215,12 @@ def editar_soporte():
 		response.flash = 'Por favor rellene el formulario'
     return dict(f=form)
 
-
-
-
-################################################### < LISTADOS > ########################################################################
-
-def listadoSoportes():
-    datosSoportes = db((db.soportes_tecnicos.tecnico_asignado==db.tecnicos.id) & (db.soportes_tecnicos.cliente==db.clientes.id)).select(db.soportes_tecnicos.ALL, db.tecnicos.ALL, db.clientes.ALL)
-    i=0
-    for x in datosSoportes:
-         i=i+1
-    return dict (datos=datosSoportes, cantidad=i)
-
-def listadoClientes():
-    datosClientes = db((db.clientes.localidad==db.localidades.id)&(db.clientes.panel==db.paneles.id)&(db.clientes.tipo_de_plan==db.planes.id)).select(db.clientes.ALL, db.localidades.localidad, db.paneles.panel, db.planes.ALL)
-    i=0
-    for x in datosClientes:
-         i=i+1
-    return dict (datos=datosClientes, cantidad=i)
-
-def listadoNodos():
-    datosNodos = db(db.nodos.localidad==db.localidades.id).select(db.nodos.ALL, db.localidades.localidad, db.localidades.id)
-    i=0
-    for x in datosNodos:
-         i=i+1
-    return dict (datos=datosNodos, cantidad=i)
-
-def listadoPaneles():
-    datosPaneles = db(db.paneles.nodo==db.nodos.id).select(db.paneles.ALL, db.nodos.ALL, db.nodos.id)
-    i=0
-    for x in datosPaneles:
-         i=i+1
-    return dict (datos=datosPaneles, cantidad=i)
-
-def listadoCostos_instalaciones():
-    datosCostos_instalaciones = db().select(db.costos_instalaciones.ALL)
-    i=0
-    for x in datosCostos_instalaciones:
-         i=i+1
-    return dict (datos=datosCostos_instalaciones, cantidad=i)
-
-def listadoCostos_soportes():
-    datosCostos_soportes = db().select(db.costos_soportes.ALL)
-    i=0
-    for x in datosCostos_soportes:
-         i=i+1
-    return dict (datos=datosCostos_soportes, cantidad=i)
-
-def listadoInstalaciones():
-    datosInstalaciones = db((db.instalaciones.localidad == db.localidades.id)&(db.instalaciones.tipo_de_plan == db.planes.id)&(db.instalaciones.costo_de_instalacion==db.costos_instalaciones.id)&(db.instalaciones.tecnico_asignado==db.tecnicos.id)).select(db.instalaciones.ALL, db.localidades.ALL, db.planes.ALL, db.costos_instalaciones.ALL, db.tecnicos.ALL)
-    i=0
-    for x in datosInstalaciones:
-         i=i+1
-    return dict (datos=datosInstalaciones, cantidad=i)
-
-
-def listadoPlanes():
-    datosPlanes = db().select(db.planes.ALL)
-    i=0
-    for x in datosPlanes:
-         i=i+1
-    return dict (datos=datosPlanes, cantidad=i)
-
-def listadoHistoriales():
-    datosHistorial = db().select(db.historiales.ALL)
-    i=0
-    for x in datosHistorial:
-         i=i+1
-    return dict (datos=datosHistorial, cantidad=i)
-
-def instalacionesDetalles():
-    instalacion_id = request.args[0]
-    resultado = db((db.instalaciones.id == instalacion_id)&(db.instalaciones.localidad == db.localidades.id)&(db.instalaciones.tipo_de_plan == db.planes.id)&(db.instalaciones.costo_de_instalacion == db.costos_instalaciones.id)&(db.instalaciones.tecnico_asignado == db.tecnicos.id)).select(db.localidades.ALL, db.planes.ALL, db.costos_instalaciones.ALL, db.instalaciones.ALL, db.tecnicos.ALL)
+def solicitudesDetalles():
+    id_solicitud = request.args[0]
+    resultado = db((db.solicitudes_instalacion.id == id_solicitud)&(db.solicitudes_instalacion.localidad == db.localidades.id)&(db.solicitudes_instalacion.tipo_de_plan == db.planes.id)&(db.solicitudes_instalacion.costo_de_instalacion == db.costos_instalaciones.id)&(db.solicitudes_instalacion.tecnico_asignado == db.tecnicos.id)).select(db.localidades.ALL, db.planes.ALL, db.costos_instalaciones.ALL, db.solicitudes_instalacion.ALL, db.tecnicos.ALL)
     return dict(datos=resultado)
 
 def clientesDetalles():
-    cliente_id = request.args[0]
-    resultado = db((db.clientes.id == cliente_id)&(db.clientes.localidad == db.localidades.id)&(db.clientes.tipo_de_plan == db.planes.id)&(db.paneles.nodo == db.nodos.id)&(db.clientes.panel == db.paneles.id)).select(db.localidades.ALL, db.planes.ALL, db.costos_instalaciones.ALL, db.clientes.ALL, db.nodos.ALL, db.paneles.ALL)
+    id_cliente = request.args[0]
+    resultado = db((db.clientes.id == id_cliente)&(db.clientes.localidad == db.localidades.id)&(db.clientes.tipo_de_plan == db.planes.id)&(db.paneles.nodo == db.nodos.id)&(db.clientes.panel == db.paneles.id)).select(db.localidades.ALL, db.planes.ALL, db.costos_instalaciones.ALL, db.clientes.ALL, db.nodos.ALL, db.paneles.ALL)
     return dict(datos=resultado)
