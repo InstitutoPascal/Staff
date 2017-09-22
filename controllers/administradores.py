556f2030c3a91da0288530d261c7084d2d9ee54a
Hernan Robles <robleshernan91@gmail.com>
2017-09-22 11:30:58 -0300

    Se agrega la función generar_pdf en el controlador administradores.py fixes #196

--- controllers/administradores.py
+++ controllers/administradores.py
@@ -224,3 +224,182 @@ def clientesDetalles():
     id_cliente = request.args[0]
     resultado = db((db.clientes.id == id_cliente)&(db.clientes.localidad==db.localidades.id)&(db.clientes.tipo_de_plan==db.planes.id)&(db.clientes.numero_de_instalacion == db.instalaciones.id)&(db.instalaciones.panel == db.paneles.id)).select(db.clientes.ALL, db.localidades.localidad, db.paneles.ALL, db.planes.ALL, db.instalaciones.ALL)
     return dict(datos=resultado)
+
+
+##################################################################
+
+import datetime
+from ConfigParser import SafeConfigParser
+from pyafipws.pyfepdf import FEPDF
+
+def generar_pdf():
+
+    CONFIG_FILE = "ubicacion del archivo rece.ini"
+
+    config = SafeConfigParser()
+    config.read(CONFIG_FILE)
+    conf_fact = dict(config.items('FACTURA'))
+    conf_pdf = dict(config.items('PDF'))
+
+
+    fepdf = FEPDF()
+    # cargo el formato CSV por defecto (factura.csv)
+    fepdf.CargarFormato(conf_fact.get("formato", "factura.csv"))
+    # establezco formatos (cantidad de decimales) según configuración:
+    fepdf.FmtCantidad = conf_fact.get("fmt_cantidad", "0.2")
+    fepdf.FmtPrecio = conf_fact.get("fmt_precio", "0.2")
+
+    # creo una factura de ejemplo
+    HOMO = True
+
+    # datos generales del encabezado:
+    tipo_cbte = 1
+    punto_vta = 4000
+    fecha = datetime.datetime.now().strftime("%Y%m%d")
+    concepto = 3
+    tipo_doc = 80; nro_doc = "30000000007"
+    cbte_nro = 12345678
+    imp_total = "127.00"
+    imp_tot_conc = "3.00"
+    imp_neto = "100.00"
+    imp_iva = "21.00"
+    imp_trib = "1.00"
+    imp_op_ex = "2.00"
+    imp_subtotal = "105.00"
+    fecha_cbte = fecha
+    fecha_venc_pago = fecha
+    # Fechas del período del servicio facturado (solo si concepto> 1)
+    fecha_serv_desde = fecha
+    fecha_serv_hasta = fecha
+    # campos p/exportación (ej): DOL para USD, indicando cotización:
+    moneda_id = 'PES'
+    moneda_ctz = 1
+    incoterms = 'FOB'                   # solo exportación
+    idioma_cbte = 1                     # 1: es, 2: en, 3: pt
+
+    # datos adicionales del encabezado:
+    nombre_cliente = 'Nestor Ortigoza'
+    domicilio_cliente = 'Rua 76 km 34.5 Alagoas'
+    pais_dst_cmp = 212                  # 200: Argentina, ver tabla
+    id_impositivo = 'PJ54482221-l'      # cat. iva (mercado interno)
+    forma_pago = '30 dias'
+
+    obs_generales = "Observaciones Generales<br/>linea2<br/>linea3"
+    obs_comerciales = "Observaciones Comerciales<br/>texto libre"
+
+    # datos devueltos por el webservice (WSFEv1, WSMTXCA, etc.):
+    motivo_obs = "Factura individual, DocTipo: 80, DocNro 30000000007 no se encuentra registrado en los padrones de AFIP."
+    cae = "61123022925855"
+    fch_venc_cae = "20110320"
+
+    fepdf.CrearFactura(concepto, tipo_doc, nro_doc, tipo_cbte, punto_vta,
+                cbte_nro, imp_total, imp_tot_conc, imp_neto,
+                imp_iva, imp_trib, imp_op_ex, fecha_cbte, fecha_venc_pago, 
+                fecha_serv_desde, fecha_serv_hasta, 
+                moneda_id, moneda_ctz, cae, fch_venc_cae, id_impositivo,
+                nombre_cliente, domicilio_cliente, pais_dst_cmp, 
+                obs_comerciales, obs_generales, forma_pago, incoterms, 
+                idioma_cbte, motivo_obs)
+
+    # completo campos extra del encabezado:
+    ok = fepdf.EstablecerParametro("localidad_cliente", "Hurlingham")
+    ok = fepdf.EstablecerParametro("provincia_cliente", "Buenos Aires")
+
+    # imprimir leyenda "Comprobante Autorizado" (constatar con WSCDC!)
+    ok = fepdf.EstablecerParametro("resultado", "A")
+
+    # tributos adicionales:
+    tributo_id = 99
+    desc = 'Impuesto Municipal Matanza'
+    base_imp = "100.00"
+    alic = "1.00"
+    importe = "1.00"
+    fepdf.AgregarTributo(tributo_id, desc, base_imp, alic, importe)
+
+    tributo_id = 4
+    desc = 'Impuestos Internos'
+    base_imp = None
+    alic = None
+    importe = "0.00"
+    fepdf.AgregarTributo(tributo_id, desc, base_imp, alic, importe)
+
+    # subtotales por alícuota de IVA:
+    iva_id = 5 # 21%
+    base_imp = 100
+    importe = 21
+    fepdf.AgregarIva(iva_id, base_imp, importe)
+
+
+    # detalle de artículos:
+    u_mtx = 123456
+    cod_mtx = 1234567890123
+    codigo = "P0001"
+    ds = "Descripcion del producto P0001\n"
+    qty = 1.00
+    umed = 7
+    if tipo_cbte in (1, 2, 3, 4, 5, 34, 39, 51, 52, 53, 54, 60, 64):
+                # discriminar IVA si es clase A / M
+        precio = 110.00
+        imp_iva = 23.10
+    else:
+        # no discriminar IVA si es clase B (importe final iva incluido)
+        precio = 133.10
+        imp_iva = None
+    bonif = 0.00
+    iva_id = 5
+    importe = 133.10
+    despacho = u'Nº 123456'
+    dato_a = "Dato A"
+    fepdf.AgregarDetalleItem(u_mtx, cod_mtx, codigo, ds, qty, umed, 
+                precio, bonif, iva_id, imp_iva, importe, despacho, dato_a)
+
+    # descuento general (a tasa 21%):
+    u_mtx = cod_mtx = codigo = None
+    ds = u"Bonificación/Descuento 10%"
+    qty = precio = bonif = None
+    umed = 99
+    iva_id = 5
+    if tipo_cbte in (1, 2, 3, 4, 5, 34, 39, 51, 52, 53, 54, 60, 64):
+        # discriminar IVA si es clase A / M
+        imp_iva = -2.21
+    else:
+        imp_iva = None
+    importe = -12.10
+    fepdf.AgregarDetalleItem(u_mtx, cod_mtx, codigo, ds, qty, umed, 
+                precio, bonif, iva_id, imp_iva, importe, "")
+
+    # descripción (sin importes ni cantidad):
+    u_mtx = cod_mtx = codigo = None
+    qty = precio = bonif = iva_id = imp_iva = importe = None
+    umed = 0
+    ds = u"Descripción Ejemplo"
+    fepdf.AgregarDetalleItem(u_mtx, cod_mtx, codigo, ds, qty, umed, 
+            precio, bonif, iva_id, imp_iva, importe, "")
+
+
+    # completo campos personalizados de la plantilla:
+    fepdf.AgregarDato("custom-nro-cli", "Cod.123")
+    fepdf.AgregarDato("custom-pedido", "1234")
+    fepdf.AgregarDato("custom-remito", "12345")
+    fepdf.AgregarDato("custom-transporte", "Camiones Ej.")
+    print "Prueba!"
+
+
+    # datos fijos:
+    for k, v in conf_pdf.items():
+        fepdf.AgregarDato(k, v)
+        if k.upper() == 'CUIT':
+            fepdf.CUIT = v  # CUIT del emisor para código de barras
+
+    fepdf.CrearPlantilla(papel=conf_fact.get("papel", "legal"), 
+                         orientacion=conf_fact.get("orientacion", "portrait"))
+    fepdf.ProcesarPlantilla(num_copias=int(conf_fact.get("copias", 1)),
+                            lineas_max=int(conf_fact.get("lineas_max", 24)),
+                            qty_pos=conf_fact.get("cant_pos") or 'izq')
+
+    salida = "/tmp/factura.pdf"
+    fepdf.GenerarPDF(archivo=salida)
+
+    response.headers['Content-Type'] = "application/pdf"
+
+    return open(salida, "rb")
