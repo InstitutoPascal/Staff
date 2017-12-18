46ff17b5dd1ec27f3f997fec5cf69c75447564b0
Emanuel Carabajal <emanuelcarabajal9@gmail.com>
2017-12-18 09:29:59 -0300

    se eliminan funciones de gerentes.py fixes #271

--- controllers/gerentes.py
+++ controllers/gerentes.py
@@ -129,67 +129,6 @@ def alta_localidad():
         response.flash = 'Complete el formulario'
     return dict(f=form)
 
-def alta_productos():
-    form = SQLFORM(db.productos)
-    if form.accepts(request.vars, session):
-        response.flash = 'Formulario aceptado'
-    elif form.errors:
-        response.flash = 'El formulario tiene errores'
-    else:
-        response.flash = 'Complete el formulario'
-    return dict(f=form)
-
-def alta_proveedores():
-    form = SQLFORM(db.proveedores)
-    if form.accepts(request.vars, session):
-        response.flash = 'Formulario aceptado'
-    elif form.errors:
-        response.flash = 'El formulario tiene errores'
-    else:
-        response.flash = 'Complete el formulario'
-    return dict(f=form)
-
-def alta_compra():
-    form = SQLFORM(db.compras)
-    if form.accepts(request.vars, session):
-        response.flash = 'Formulario aceptado'
-        redirect(URL(c="gerentes", f="calcularTotalCompra"))
-    elif form.errors:
-        response.flash = 'El formulario tiene errores'
-    else:
-        response.flash = 'Complete el formulario'
-    return dict(f=form)
-
-def calcularTotalCompra():
-    id_ultima_compra = db().select(db.compras.id).last().id
-    ultimo_producto = db().select(db.compras.producto).last().producto
-    ultima_fecha = db().select(db.compras.fecha_de_compra).last().fecha_de_compra
-    ultimo_precio = db().select(db.compras.precio_de_compra).last().precio_de_compra
-    ultima_cantidad = db().select(db.compras.cantidad).last().cantidad
-    precio_utilizado = ultima_cantidad * ultimo_precio
-    db(db.compras.id == id_ultima_compra).update(total=precio_utilizado)
-    redirect(URL(c="gerentes", f="AgregarStock"))
-
-def AgregarStock():
-    ultimo_producto = db().select(db.compras.producto).last().producto
-    ultima_fecha = db().select(db.compras.fecha_de_compra).last().fecha_de_compra
-    ultimo_precio = db().select(db.compras.precio_de_compra).last().precio_de_compra
-    ultima_cantidad = db().select(db.compras.cantidad).last().cantidad
-    precio_utilizado = ultima_cantidad * ultimo_precio
-    registros = db(db.stock.detalle==ultimo_producto).select()
-    if registros:
-            ultima_cantidad_stock = db(db.stock.detalle==ultimo_producto).select(db.stock.cantidad_existencia).last().cantidad_existencia
-            ultimo_total_stock = db(db.stock.detalle==ultimo_producto).select(db.stock.total_existencia).last().total_existencia
-            nuevo_stock = ultima_cantidad_stock + ultima_cantidad
-            nuevo_total_stock = ultimo_total_stock + precio_utilizado
-            nuevo_precio_unitario_stock = nuevo_total_stock / nuevo_stock
-            db.stock.insert(fecha=ultima_fecha, detalle=ultimo_producto, cantidad_entrada=ultima_cantidad, costo_unitario_entrada=ultimo_precio, total_entrada=precio_utilizado, cantidad_existencia=nuevo_stock, costo_unitario_existencia=nuevo_precio_unitario_stock, total_existencia=nuevo_total_stock)
-            redirect(URL(c="gerentes", f="alta_compra"))
-    else:
-        unitario_existencia = precio_utilizado/ultima_cantidad
-        db.stock.insert(fecha=ultima_fecha, detalle=ultimo_producto, cantidad_entrada=ultima_cantidad, costo_unitario_entrada=ultimo_precio, total_entrada=precio_utilizado, cantidad_existencia=ultima_cantidad, costo_unitario_existencia=unitario_existencia, total_existencia=precio_utilizado)
-        redirect(URL(c="gerentes", f="alta_compra"))
-
 ######################################### REGISTROS COMPLETOS #########################################
 
 def listadoAdministradores():
@@ -247,34 +186,6 @@ def listadoLocalidades():
     for x in datosLocalidades:
          i=i+1
     return dict (datos=datosLocalidades, cantidad=i)
-
-def listadoProductos():
-    datosProductos = db().select(db.productos.ALL)
-    i=0
-    for x in datosProductos:
-         i=i+1
-    return dict (datos=datosProductos, cantidad=i)
-
-def listadoProveedores():
-    datosProveedores = db(db.proveedores.localidad == db.localidades.id).select(db.proveedores.ALL, db.localidades.ALL)
-    i=0
-    for x in datosProveedores:
-         i=i+1
-    return dict (datos=datosProveedores, cantidad=i)
-
-def listadoCompras():
-    datosCompras = db((db.compras.proveedor==db.proveedores.id)&(db.compras.producto==db.productos.id)).select(db.compras.ALL, db.proveedores.ALL, db.productos.ALL)
-    i=0
-    for x in datosCompras:
-         i=i+1
-    return dict (datos=datosCompras, cantidad=i)
-
-def listadoStock():
-    datosStock = db(db.stock.detalle==db.productos.id).select(db.stock.ALL, db.productos.ALL)
-    i=0
-    for x in datosStock:
-         i=i+1
-    return dict (datos=datosStock, cantidad=i)
 
 ######################################### ESTADISTICAS #########################################
 
